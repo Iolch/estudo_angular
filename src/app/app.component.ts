c38f2da2e34c677ab5e658d371f8859ce6adf47a
Iolanda Chagas <iccpaiva@gmail.com>
2019-08-02 13:30:43 -0300

    Básico de data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'meu-app';
-  name = '';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log("Preparing to allow Toogle");
+  }
+  ServerName: String = "Apollo";
+  ServerPID = 11;
+  ServerStatus = "offline";
+  statusFlag = false;
+  allowToogle = false;
+
+  constructor(){
+    //              passando uma função
+    //         -------------^--------------
+    setTimeout(()=>{this.allowToogle = true}, 5000);
+    
+  }
+
+  toggleServerStatus():void {
+    
+    this.statusFlag = !this.statusFlag;
+    if(this.statusFlag) this.ServerStatus = "online";
+    else this.ServerStatus = "offline";
+  }
+
+  
+  
 }
